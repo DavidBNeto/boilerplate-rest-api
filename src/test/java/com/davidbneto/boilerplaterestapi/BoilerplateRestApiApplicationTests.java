12061875cf2237fb0891511ebbd6457f31b4ab28
DavidBNeto <netodavid25@hotmail.com>
2022-08-07 20:01:44 -0300

    initial commit

---
+++ src/test/java/com/davidbneto/boilerplaterestapi/BoilerplateRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidbneto.boilerplaterestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateRestApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
